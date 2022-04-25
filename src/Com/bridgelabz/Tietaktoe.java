dfedf1ad506a3b80133bed718388a0f564ae2612
Priyanka <priyankahansi95@gmail.com>
2022-04-25 13:53:59 +0530

    added user input

--- src/Com/bridgelabz/Tietaktoe.java
+++ src/Com/bridgelabz/Tietaktoe.java
@@ -1,14 +1,40 @@
 package Com.bridgelabz;
 
+import java.io.StringReader;
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tietaktoe {
     static void board() {
         char[] arr = new char[10];
         for (int i = 1; i < 9; i++) {
             arr[i] = ' ';
         }
-    }
 
+    }
+    static void input () {
+
+        System.out.println("enter X and O as a input");
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        if (input == 'X') {
+            System.out.println("the value is X");
+        }
+        else if(input=='O')
+        {
+            System.out.println("the value is O");
+        }
+        else
+        {
+            System.out.println("enter a valid input");
+        }
+    }
     public static void main(String[] args) {
         board();
+        input();
     }
 }
+
+            // public static void main(String[] args) {
+
